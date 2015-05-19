0a9f6c331d08ce8612ab74ec4230d94755ffc5a1
mkotlyar <mikhail.katliar@tuebingen.mpg.de>
2015-05-19 18:00:16 +0200

    Added generic MotionPlatform model.

--- controller.cpp
+++ controller.cpp
@@ -6,7 +6,6 @@
 #define NUM_INPUTS          2
 /* Input Port  0 */
 #define IN_PORT_0_NAME      y_ref
-#define INPUT_0_WIDTH       1
 #define INPUT_DIMS_0_COL    1
 #define INPUT_0_DTYPE       real_T
 #define INPUT_0_COMPLEX     COMPLEX_NO
@@ -23,7 +22,6 @@
 #define IN_0_SLOPE           0.125
 /* Input Port  1 */
 #define IN_PORT_1_NAME      x
-#define INPUT_1_WIDTH       2
 #define INPUT_DIMS_1_COL    1
 #define INPUT_1_DTYPE       real_T
 #define INPUT_1_COMPLEX     COMPLEX_NO
@@ -42,7 +40,6 @@
 #define NUM_OUTPUTS          1
 /* Output Port  0 */
 #define OUT_PORT_0_NAME      u
-#define OUTPUT_0_WIDTH       1
 #define OUTPUT_DIMS_0_COL    1
 #define OUTPUT_0_DTYPE       real_T
 #define OUTPUT_0_COMPLEX     COMPLEX_NO
@@ -77,6 +74,8 @@
 #include "simstruc.h"
 
 #include <qpDUNES.h>
+#include <Eigen/Dense>
+
 /*
 extern "C"
 {
@@ -95,17 +94,139 @@ extern "C"
 
 #include <memory>
 #include <array>
+#include <vector>
 
-// Motion platform parameters.
-const unsigned numberOfAxes = 1;
-const unsigned Nx = numberOfAxes * 2;
-const unsigned Nu = numberOfAxes;
-const unsigned Nz = Nx + Nu;
-const unsigned Ny = 1;
-const unsigned Nt = 3; // number of control intervals
+class System
+{
+public:
+	unsigned getInputDim() const
+	{
+		return _inputDim;
+	}
 
-const std::array<double, Nz> z_min = {-1, -1, -1};
-const std::array<double, Nz> z_max = {-1, -1, -1};
+	unsigned getStateDim() const
+	{
+		return _stateDim;
+	}
+
+	unsigned getOutputDim() const
+	{
+		return _outputDim;
+	}
+
+protected:
+	System(unsigned nu, unsigned nx, unsigned ny)
+		: _inputDim(nu), _stateDim(nx), _outputDim(ny)
+	{
+	}
+
+private:
+	unsigned _inputDim;
+	unsigned _stateDim;
+	unsigned _outputDim;
+};
+
+class MotionLimits
+{
+public:
+	MotionLimits(double q_min, double q_max, double v_min, double v_max, double u_min, double u_max)
+		: _qMin(q_min), _qMax(q_max), _vMin(v_min), _vMax(v_max), _uMin(u_min), _uMax(u_max)
+	{
+		if (!(q_min < q_max && v_min < v_max && u_min < u_max))
+			throw std::invalid_argument("MotionLimits::MotionLimits(): invalid motion limits (min must be less than max)");
+	}
+
+private:
+	double _qMin;
+	double _qMax;
+	double _vMin;
+	double _vMax;
+	double _uMin;
+	double _uMax;
+};
+
+class MotionPlatform : public System
+{
+public:
+	MotionPlatform(const std::vector<MotionLimits> limits)
+		: System(static_cast<unsigned>(limits.size()), static_cast<unsigned>(2 * limits.size()), 6)
+		, _gravity(0., 0., -9.81)
+	{
+	}
+
+	unsigned getNumberOfAxes() const
+	{
+		return static_cast<unsigned>(_axesLimits.size());
+	}
+
+	const std::vector<MotionLimits>& getAxesLimits() const
+	{
+		return _axesLimits;
+	}
+
+	const Eigen::Vector3d& getGravity() const
+	{
+		return _gravity;
+	}
+
+	void setGravity(const Eigen::Vector3d& g)
+	{
+		_gravity = g;
+	}
+
+	typedef Eigen::Map<Eigen::VectorXd> VectorMap;
+	typedef Eigen::Map<Eigen::MatrixXd, 0, Eigen::Stride<Eigen::Dynamic, Eigen::Dynamic>> MatrixMap;
+
+	virtual void Output(const VectorMap& x, const VectorMap& u, VectorMap& y, MatrixMap& C, MatrixMap& D) const = 0;
+
+private:
+	std::vector<MotionLimits> _axesLimits;
+	Eigen::Vector3d _gravity;
+};
+
+class MotionPlatformX : public MotionPlatform
+{
+public:
+	MotionPlatformX()
+		: MotionPlatform(DefaultAxesLimits())
+	{
+	}
+
+	void Output(const VectorMap& x, const VectorMap& u, VectorMap& y, MatrixMap& C, MatrixMap& D) const override
+	{
+		if (!(x.size() == getStateDim() && u.size() == getInputDim() && y.size() == getOutputDim() 
+			&& C.rows() == getOutputDim() && C.cols() == getStateDim() && D.rows() == getOutputDim() && D.cols() == getInputDim()))
+			throw std::invalid_argument("MotionPlatformX::Output() input argument dimension mismatch");
+
+		// Specific force
+		y.block<3, 1>(0, 0) = getGravity();
+		y(0) -= u(0);
+
+		// Rotational velocity
+		y.block<3, 1>(3, 0).fill(0.);
+
+		// C = dy/dx
+		C.fill(0.);
+
+		// D = dy/du
+		D.fill(0.);
+		D(0, 0) = -1;
+	}
+
+private:
+	static std::vector<MotionLimits> DefaultAxesLimits()
+	{
+		std::vector<MotionLimits> limits;
+		limits.push_back(MotionLimits(-1., 1., -1., 1., -1., 1.));
+		return limits;
+	}
+};
+
+// Motion platform to be used.
+MotionPlatformX platform;
+
+// number of control intervals
+const unsigned Nt = 3; 
 
 extern void S_Outputs_wrapper(const real_T *y_ref,
 			const real_T *x,
@@ -127,7 +248,6 @@ extern void S_Update_wrapper(const real_T *y_ref,
  */
 static void mdlInitializeSizes(SimStruct *S)
 {
-
     DECL_AND_INIT_DIMSINFO(inputDimsInfo);
     DECL_AND_INIT_DIMSINFO(outputDimsInfo);
     ssSetNumSFcnParams(S, NPARAMS);
@@ -140,14 +260,14 @@ static void mdlInitializeSizes(SimStruct *S)
 
     if (!ssSetNumInputPorts(S, NUM_INPUTS)) return;
     /*Input Port 0 */
-    ssSetInputPortWidth(S,  0, INPUT_0_WIDTH); /* */
+    ssSetInputPortWidth(S,  0, platform.getOutputDim()); /* */
     ssSetInputPortDataType(S, 0, SS_DOUBLE);
     ssSetInputPortComplexSignal(S,  0, INPUT_0_COMPLEX);
     ssSetInputPortDirectFeedThrough(S, 0, INPUT_0_FEEDTHROUGH);
     ssSetInputPortRequiredContiguous(S, 0, 1); /*direct input signal access*/
 
     /*Input Port 1 */
-    ssSetInputPortWidth(S,  1, INPUT_1_WIDTH);
+    ssSetInputPortWidth(S,  1, platform.getStateDim());
     ssSetInputPortDataType(S, 1, SS_DOUBLE);
     ssSetInputPortComplexSignal(S, 1, INPUT_1_COMPLEX);
     ssSetInputPortDirectFeedThrough(S, 1, INPUT_1_FEEDTHROUGH);
@@ -155,7 +275,7 @@ static void mdlInitializeSizes(SimStruct *S)
 
 
     if (!ssSetNumOutputPorts(S, NUM_OUTPUTS)) return;
-    ssSetOutputPortWidth(S, 0, OUTPUT_0_WIDTH);
+    ssSetOutputPortWidth(S, 0, platform.getInputDim());
     ssSetOutputPortDataType(S, 0, SS_DOUBLE);
     ssSetOutputPortComplexSignal(S, 0, OUTPUT_0_COMPLEX);
     ssSetNumSampleTimes(S, 1);
@@ -196,17 +316,34 @@ static void mdlInitializeSampleTimes(SimStruct *S)
   */
  static void mdlInitializeConditions(SimStruct *S)
  {
-     auto qpData = reinterpret_cast<qpData_t *>(ssGetPWorkValue(S, 0));
-     
-     /** Initial MPC data setup: components not given here are set to zero (if applicable)
-	 *      instead of passing g, D, zLow, zUpp, one can also just pass NULL pointers (0) */
-    
-	 return_t statusFlag = qpDUNES_init(qpData, H, g, C, c, z_min.data(), z_max.data(), 0, 0, 0);
-	 if (statusFlag != QPDUNES_OK) 
-     {
-  		 ssSetErrorStatus(S, "qpDUNES_init() failed.");
-		 return;
-	 }
+// 	 // G = [dy/dx, dy/du]
+// 	 Eigen::Matrix<double, Ny, Nz> G;
+// 	 G << 0. << 0. << 1.;
+// 
+// 	 // Initializing matrices.
+// 	 // H stores Nt matrices of size Nz x Nz and 1 matrix of size Nx x Nx.
+// 	 std::array<double, Nz * Nz * Nt + Nx * Nx> H;
+// 
+// 	 // g stores Nt vectors of size Nz and 1 vector of size Nx
+// 	 std::array<double, Nz * Nt + Nx> g;
+// 
+// 	 for (unsigned i = 0; i < Nt; ++i)
+// 	 {
+// 		 Eigen::Map<Eigen::Matrix<double, Nz, Nz, Eigen::RowMajor>>(H.data() + Nz * Nz * i) = G.transpose() * G;
+// 		 Eigen::Map<Eigen::Matrix<double, Nz, 1>>(g.data() + Nz * i) = G.transpose() * G;
+// 	 }
+// 
+//      auto qpData = reinterpret_cast<qpData_t *>(ssGetPWorkValue(S, 0));
+//      
+//      /** Initial MPC data setup: components not given here are set to zero (if applicable)
+// 	 *      instead of passing g, D, zLow, zUpp, one can also just pass NULL pointers (0) */
+// 	 
+// 	 return_t statusFlag = qpDUNES_init(qpData, H, g, C, c, z_min.data(), z_max.data(), 0, 0, 0);
+// 	 if (statusFlag != QPDUNES_OK) 
+//      {
+//   		 ssSetErrorStatus(S, "qpDUNES_init() failed.");
+// 		 return;
+// 	 }
  }
 
 #define MDL_START  /* Change to #undef to remove function */
@@ -229,7 +366,7 @@ static void mdlStart(SimStruct *S)
     auto qpData = std::make_unique<qpData_t>();
     
     unsigned int* nD = 0;	  			/* number of affine constraints */
-	return_t statusFlag = qpDUNES_setup(qpData.get(), Nt, Nx, Nu, nD, &qpOptions);
+	return_t statusFlag = qpDUNES_setup(qpData.get(), Nt, platform.getStateDim(), platform.getInputDim(), nD, &qpOptions);
 	if (statusFlag != QPDUNES_OK)
 	{
 		ssSetErrorStatus(S, "qpDUNES_setup() failed.");
@@ -257,6 +394,7 @@ static void mdlSetDefaultPortDataTypes(SimStruct *S)
   ssSetInputPortDataType( S, 0, SS_DOUBLE);
   ssSetOutputPortDataType(S, 0, SS_DOUBLE);
 }
+
 /* Function: mdlOutputs =======================================================
  *
 */
@@ -264,14 +402,12 @@ static void mdlOutputs(SimStruct *S, int_T tid)
 {
     const real_T   *y_ref  = (const real_T*) ssGetInputPortSignal(S,0);
     const real_T   *x  = (const real_T*) ssGetInputPortSignal(S,1);
-    real_T        *u  = (real_T *)ssGetOutputPortRealSignal(S,0);
-    const real_T   *xD = ssGetDiscStates(S);
+    real_T         *u  = (real_T *)ssGetOutputPortRealSignal(S,0);
 
-
-    S_Outputs_wrapper(y_ref, x, u, xD, S);
-
-
+    //S_Outputs_wrapper(y_ref, x, u, xD, S);
+	u[0] = -y_ref[0];
 }
+
 #define MDL_UPDATE  /* Change to #undef to remove function */
 /* Function: mdlUpdate ======================================================
    * Abstract:
