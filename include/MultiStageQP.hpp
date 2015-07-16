c279eb000633ce3899a05077761e2da94fd52484
mkotlyar <mikhail.katliar@tuebingen.mpg.de>
2015-07-16 18:18:12 +0200

    * Implemented condensed g vector calculation. * Wrote a test for condensed H matrix correctness. The test does not pass yet.

--- include/MultiStageQP.hpp
+++ include/MultiStageQP.hpp
@@ -33,38 +33,58 @@ namespace camels
 			Eigen::MatrixXd M(_Nx, nIndep());
 			M.setZero();
 
-			Eigen::VectorXd v(_Nz);
+			Eigen::VectorXd v(_Nx);
 			v.setZero();
 
 			Hc.setZero();
+			gc.setZero();
 
-			for (unsigned k = 0; k < _Nt; ++k)
+			for (unsigned k = 0; k <= _Nt; ++k)
 			{
 				auto M_k = M.leftCols(_Nx + k * _Nu);
 				if (k == 0)
 				{
 					M_k.setIdentity();
+					v.setZero();
 				}
 				else
 				{
-					M_k = C(k).leftCols(_Nx) * M_k;
-					M_k.rightCols(_Nu) = C(k).rightCols(_Nu);
+					const auto A_k_minus = C(k - 1).leftCols(_Nx);
+					const auto B_k_minus = C(k - 1).rightCols(_Nu);
+					M_k = A_k_minus * M_k;
+					M_k.rightCols(_Nu) = B_k_minus;
+					v = A_k_minus * v + c(k - 1);
 				}
 
 				const auto H_k = H(k);
-				const auto Q  = H_k.topLeftCorner    (_Nx, _Nx);
-				const auto S  = H_k.topRightCorner   (_Nx, _Nu);
-				const auto ST = H_k.bottomLeftCorner (_Nu, _Nx);
-				const auto R  = H_k.bottomRightCorner(_Nu, _Nu);
+				const auto g_k = g(k);
 
-				const auto nn = M_k.cols();
-				auto Hc_k = Hc.topLeftCorner(nn + _Nu, nn + _Nu);
-				Hc_k.topLeftCorner    (nn , nn ) += M_k.transpose() * Q * M_k;
-				Hc_k.topRightCorner   (nn , _Nu) += M_k.transpose() * S;
-				Hc_k.bottomLeftCorner (_Nu, nn ) += ST * M_k;
-				Hc_k.bottomRightCorner(_Nu, _Nu) += R;
+				if (k < _Nt)
+				{
+					const auto Q = H_k.topLeftCorner(_Nx, _Nx);
+					const auto S = H_k.topRightCorner(_Nx, _Nu);
+					const auto ST = H_k.bottomLeftCorner(_Nu, _Nx);
+					const auto R = H_k.bottomRightCorner(_Nu, _Nu);
+
+					const auto nn = M_k.cols();
+					auto Hc_k = Hc.topLeftCorner(nn + _Nu, nn + _Nu);
+					Hc_k.topLeftCorner(nn, nn) += M_k.transpose() * Q * M_k;
+					Hc_k.topRightCorner(nn, _Nu) += M_k.transpose() * S;
+					Hc_k.bottomLeftCorner(_Nu, nn) += ST * M_k;
+					Hc_k.bottomRightCorner(_Nu, _Nu) += R;
+
+					auto gc_k = gc.topRows(nn + _Nu);
+					gc_k.topRows(nn) += M_k.transpose() * (g_k.topRows(_Nx) + (Q + Q.transpose()) * v);
+					gc_k.bottomRows(_Nu) += g_k.bottomRows(_Nu) + (S.transpose() + ST) * v;
+				}
+				else
+				{
+					// Final state.
+					Hc += M_k.transpose() * H_k * M_k;
+					gc += M_k.transpose() * (g_k.topRows(_Nx) + (H_k + H_k.transpose()) * v);
+				}
 			}
-
+			
 // 			Eigen::SparseMatrix<double> H(nVar(), nVar());
 // 			Eigen::SparseMatrix<double> P(nVar(), nVar());
 // 
--- src/condensing_test.cpp
+++ src/condensing_test.cpp
@@ -7,20 +7,63 @@
 TEST(test_1, my_test_1)
 {
 	camels::MultiStageQP qp(2, 1, 2);
-	qp.H(0) << 1, 2, 3, 4, 5, 6, 7, 8, 9;
-	qp.H(0) = qp.H(0).transpose() * qp.H(0);
-	qp.H(1) << 1, 3, 2, 4, 6, 5, 8, 7, 9;
-	qp.H(1) = qp.H(1).transpose() * qp.H(1);
-	qp.H(2) << 1, 2, 3, 4;
-	qp.H(2) = qp.H(2).transpose() * qp.H(2);
 
-	qp.C(0) << 1, 1, 0.5,
-			   0, 1, 1.0;
-	qp.c(0) << 1, 2;
-	qp.C(1) << 1, 1, 0.5,
-			   0, 1, 1.0;
-	qp.c(1) << 1, 2;
+	// Stage 0
+	Eigen::MatrixXd H0(qp.nZ(), qp.nZ());
+	H0 << 1, 2, 3, 4, 5, 6, 7, 8, 9;
+	H0 = H0.transpose() * H0;	// Make positive definite.
 
+	const Eigen::MatrixXd Q0 = H0.topLeftCorner(qp.nX(), qp.nX());
+	const Eigen::MatrixXd R0 = H0.bottomRightCorner(qp.nU(), qp.nU());
+	const Eigen::MatrixXd S0 = H0.topRightCorner(qp.nX(), qp.nU());
+	const Eigen::MatrixXd S0T = H0.bottomLeftCorner(qp.nU(), qp.nX());
+
+	Eigen::MatrixXd A0(qp.nX(), qp.nX());
+	A0 << 1, 1, 0, 1;
+
+	Eigen::MatrixXd B0(qp.nX(), qp.nU());
+	B0 << 0.5, 1.0;
+
+	Eigen::VectorXd a0(qp.nX());
+	a0 << 1, 2;
+
+	// Stage 1
+	Eigen::MatrixXd H1(qp.nZ(), qp.nZ());
+	H1 << 1, 2, 3, 4, 5, 6, 7, 8, 9;
+	H1 = H1.transpose() * H1;	// Make positive definite.
+
+	const Eigen::MatrixXd Q1 = H1.topLeftCorner(qp.nX(), qp.nX());
+	const Eigen::MatrixXd R1 = H1.bottomRightCorner(qp.nU(), qp.nU());
+	const Eigen::MatrixXd S1 = H1.topRightCorner(qp.nX(), qp.nU());
+	const Eigen::MatrixXd S1T = H1.bottomLeftCorner(qp.nU(), qp.nX());
+
+	Eigen::MatrixXd A1(qp.nX(), qp.nX());
+	A1 << 1, 1, 0, 1;
+
+	Eigen::MatrixXd B1(qp.nX(), qp.nU());
+	B1 << 0.5, 1.0;
+
+	Eigen::VectorXd a1(qp.nX());
+	a1 << 1, 2;
+
+	// Stage 2
+	Eigen::MatrixXd H2(qp.nX(), qp.nX());
+	H2 << 1, 2, 3, 4;
+	H2 = H2.transpose() * H2;	// Make positive definite.
+
+	const Eigen::MatrixXd Q2 = H1.topLeftCorner(qp.nX(), qp.nX());
+
+	// Setup QP
+	qp.H(0) = H0;
+	qp.H(1) = H1;
+	qp.H(2) = H2;
+
+	qp.C(0) << A0, B0;
+	qp.c(0) << a0;
+	qp.C(1) << A1, B1;
+	qp.c(1) << a1;
+
+	// Condense
 	Eigen::MatrixXd M(qp.nDep(), qp.nIndep());
 	Eigen::VectorXd v(qp.nDep());
 	Eigen::SparseMatrix<double> H(qp.nVar(), qp.nVar());
@@ -30,26 +73,34 @@ TEST(test_1, my_test_1)
 
 	qp.Calculate_M(M);
 	qp.Calculate_v(v);
-	qp.Calculate_FullH(H);
-	qp.Calculate_PermutationMatrix(P);
 	qp.Condense(Hc, gc);
-	std::cout << M << std::endl;
-	std::cout << v << std::endl;
-	std::cout << H << std::endl;
-	std::cout << P << std::endl;
+// 	std::cout << M << std::endl;
+// 	std::cout << v << std::endl;
+// 	std::cout << H << std::endl;
+// 	std::cout << P << std::endl;
 	std::cout << Hc << std::endl;
+//	std::cout << gc << std::endl;
 
-	Eigen::MatrixXd M_expected(4, 4);
-	M_expected << 1, 1, 0.5,   0,
-				  0, 1,   1,   0,
-				  1, 2, 1.5, 0.5,
-				  0, 1,   1,   1;
+	Eigen::MatrixXd M_expected(qp.nDep(), qp.nIndep());
+	M_expected << 
+		A0,			B0,			Eigen::MatrixXd::Zero(qp.nX(), qp.nU()),
+		A1 * A0,	A1 * B0,	B1;
 
-	Eigen::VectorXd v_expected(4);
-	v_expected << 1, 2, 4, 4;
+	Eigen::VectorXd v_expected(qp.nIndep());
+	v_expected << 
+		a0,
+		A1 * a0 + a1;
 	
 	EXPECT_TRUE(M_expected == M);
 	EXPECT_TRUE(v_expected == v);
 
+	Eigen::MatrixXd Hc_expected(qp.nIndep(), qp.nIndep());
+	
+	Hc_expected <<
+		A0.transpose() * Q1 * A0 + A0.transpose() * A1.transpose() * Q2 * A1 * A0 + Q0,				A0.transpose() * Q1 * B0 + A0.transpose() * A1.transpose() * Q2 * A1 * B0 + S0,	A0.transpose() * S1 + A0.transpose() * A1.transpose() * Q2 * B1,
+		B0.transpose() * Q1 * A0 + B0.transpose() * A1.transpose() * Q2 * A1 * A0 + S0.transpose(), B0.transpose() * Q1 * B0 + B0.transpose() * A1.transpose() * Q2 * A1 * B0 + R0, B0.transpose() * S1 + B0.transpose() * A1.transpose() * Q2 * B1,
+		S1.transpose() * A0 + B1.transpose() * Q2 * A1 * A0,										S1.transpose() * B0 + B1.transpose() * Q2 * A1 * B0,							B1.transpose() * Q2 * B1 + R1;
+
+	std::cout << Hc_expected << std::endl;
 	//qp.PrintQP_C(std::cout);
 }
