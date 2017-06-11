ed858d3fabb028f6a7b98c9cfe27cd97c3c8c911
molivari <mario.olivari@tuebingen.mpg.de>
2017-06-11 12:41:23 +0200

    Added Eigen3 include directories

--- build2/extern/src/googletest-build/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/molivari/Work/software/projects/tmpc/build/extern/src/googletest/googlemock
-# Build directory: /home/molivari/Work/software/projects/tmpc/build/extern/src/googletest-build/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
--- build2/extern/src/qpDUNES-build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/molivari/Work/software/projects/tmpc/build/extern/src/qpDUNES
-# Build directory: /home/molivari/Work/software/projects/tmpc/build/extern/src/qpDUNES-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
