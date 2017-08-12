063514ebbdb7b707b0e1e062bc399704912ea86e
Mikhail Katliar <mikhail.katliar@tuebingen.mpg.de>
2017-08-13 00:53:40 +0200

    Added case with an equality and a bound constraint both active

--- Test.m
+++ Test.m
@@ -65,5 +65,28 @@ classdef Test < matlab.unittest.TestCase
 %             testCase.assertEqual(t, [0; 0.5], 'AbsTol', 1e-6);
             testCase.assertEqual(fval, fval1, 'AbsTol', 1e-6);
         end
+        
+        function test_Qp_2d_equality_constrained_active_bound(testCase)      
+            % 
+            qp.H = [2, 0.3; 0.3, 4];
+            qp.f = [-1; -2];
+            qp.lb = [-1.5; 1];
+            qp.ub = [3; 5];
+            qp.Aeq = [2, -0.5];
+            qp.beq = 3;
+            qp.solver = 'quadprog';
+            qp.options = optimoptions('quadprog');
+
+            [x, fval, ~, ~, lam, t] = ip_qp(qp);
+            [x1, fval1, ~, ~, lam1] = quadprog(qp);
+            
+            testCase.assertEqual(x, x1, 'AbsTol', 1e-6);
+            testCase.assertEqual(lam.eqlin, lam1.eqlin, 'AbsTol', 1e-6);
+            testCase.assertEmpty(lam.ineqlin);
+            testCase.assertEqual(lam.lower, lam1.lower, 'AbsTol', 1e-6);
+            testCase.assertEqual(lam.upper, lam1.upper, 'AbsTol', 1e-6);
+%             testCase.assertEqual(t, [0; 0.5], 'AbsTol', 1e-6);
+            testCase.assertEqual(fval, fval1, 'AbsTol', 1e-6);
+        end
     end
 end
