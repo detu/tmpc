13d02ecd309fd31dcb5e5886e19508120c0a23c8
Mikhail Katliar <cepstr@gmail.com>
2017-09-22 11:00:10 +0200

    A cosmetic change

--- src/mpc/MpcOcpSize.cpp
+++ src/mpc/MpcOcpSize.cpp
@@ -1,17 +1,16 @@
 #include <tmpc/mpc/MpcOcpSize.hpp>
 
-namespace tmpc {
-
-std::vector<OcpSize> mpcOcpSize(std::size_t nt,
-		std::size_t nx, std::size_t nu, std::size_t nc, std::size_t nct)
+namespace tmpc 
 {
-	std::vector<OcpSize> sz;
+	std::vector<OcpSize> mpcOcpSize(std::size_t nt,
+			std::size_t nx, std::size_t nu, std::size_t nc, std::size_t nct)
+	{
+		std::vector<OcpSize> sz;
 
-	sz.reserve(nt + 1);
-	std::fill_n(std::back_inserter(sz), nt, OcpSize(nx, nu, nc));
-	sz.emplace_back(nx, OcpSize::size_type{0}, nct);
+		sz.reserve(nt + 1);
+		std::fill_n(std::back_inserter(sz), nt, OcpSize(nx, nu, nc));
+		sz.emplace_back(nx, OcpSize::size_type{0}, nct);
 
-	return sz;
+		return sz;
+	}
 }
-
-}	// namespace tmpc
