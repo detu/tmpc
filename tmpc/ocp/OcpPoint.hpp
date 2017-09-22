e3c62735b041e76dd1a1213ec9701ea7ffda3895
Mikhail Katliar <cepstr@gmail.com>
2017-09-22 04:55:25 +0200

    Renamed OpPoint -> OcpPoint

---
+++ tmpc/ocp/OcpPoint.hpp
@@ -0,0 +1,61 @@
+#pragma once
+
+#include <tmpc/Matrix.hpp>
+
+namespace tmpc
+{
+	/**
+	 * \brief Point as a minimization variable of an optimization problem.
+	 */
+	template <typename Real_>
+	class OcpPoint
+	{
+	public:
+		using Real = Real_;
+
+		OcpPoint(size_t NX, size_t NU)
+		:	x_(NX)
+		,	u_(NU)
+		{
+		}
+
+		template <typename VectorX, typename VectorU>
+		OcpPoint(VectorX const& x, VectorU const& u)
+		:	x_(x)
+		,	u_(u)
+		{
+		}
+
+		template <typename T>
+		void x(T const& val)
+		{
+			x_ = val;
+		}
+
+		DynamicVector<Real> const& x() const
+		{
+			return x_;
+		}
+
+		template <typename T>
+		void u(T const& val)
+		{
+			u_ = val;
+		}
+
+		DynamicVector<Real> const& u() const
+		{
+			return u_;
+		}
+
+	private:
+		DynamicVector<Real> x_;
+		DynamicVector<Real> u_;
+	};
+
+	template <typename Real>
+	inline std::ostream& operator<<(std::ostream& os, OcpPoint<Real> const& p)
+	{
+		os << "x=" << trans(p.x()) << "\tu=" << trans(p.u());
+	}
+}
