6dd231d4f4adaf0a5645affc1c96e6f8d9d711e2
Mikhail Katliar <mikhail.katliar@tuebingen.mpg.de>
2017-09-25 17:01:08 +0200

    Added AD benchmark

--- examples/ad/ad_benchmark.cpp
+++ examples/ad/ad_benchmark.cpp
@@ -3,6 +3,8 @@
 
 #include <json.hpp>
 
+#include <benchmark/benchmark.h>
+
 #include <iostream>
 #include <tuple>
 #include <chrono>
@@ -15,8 +17,6 @@ Real constexpr M = 1.;      // mass of the cart [kg]
 Real constexpr m = 0.1;     // mass of the ball [kg]
 Real constexpr g = 9.81;    // gravity constant [m/s^2]
 Real constexpr l = 0.8;     // length of the rod [m]
-
-volatile int vv = 0;
 
 namespace tmpc
 {
@@ -54,8 +54,6 @@ void f1(Real const x[4], Real const u[1], Real dx[4])
     dx[2] = (-l * m * sin(theta) * pow(omega, 2) + F + g * m * cos(theta) * sin(theta)) / (M + m - m * pow(cos(theta), 2));
     dx[3] = (-l * m * cos(theta) * sin(theta) * pow(omega, 2) + F * cos(theta) + g * m * sin(theta) + M * g * sin(theta)) 
         / (l * (M + m - m * pow(cos(theta), 2)));
-
-    ++vv;
 }
 
 void f2(Real const x[4], Real const u[1], Real dx[4])
@@ -80,78 +78,32 @@ void f2(Real const x[4], Real const u[1], Real dx[4])
     dx[1] = eval(theta_dot, arg);
     dx[2] = eval(v_dot, arg);
     dx[3] = eval(omega_dot, arg);
-
-    ++vv;
 }
 
-int main(int, char **)
+
+static void BM_f1(benchmark::State& state) 
 {
-    using clock = std::chrono::high_resolution_clock;
-    using nanoseconds = std::chrono::duration<float, std::nano>;
-
-    /*
-    M = 1    # mass of the cart [kg]
-    m = 0.1  # mass of the ball [kg]
-    g = 9.81 # gravity constant [m/s^2]
-    l = 0.8  # length of the rod [m]
-
-    p = SX.sym('p')         # horizontal displacement [m]
-    theta = SX.sym('theta') # angle with the vertical [rad]
-    v = SX.sym('v')         # horizontal velocity [m/s]
-    omega = SX.sym('omega') # angular velocity [rad/s]
-    F = SX.sym('F')         # horizontal force [N]
-
-    ode_rhs = vertcat(v,
-                      omega,
-                      (- l*m*sin(theta)*omega**2 + F + g*m*cos(theta)*sin(theta))/(M + m - m*cos(theta)**2),
-                      (- l*m*cos(theta)*sin(theta)*omega**2 + F*cos(theta) + g*m*sin(theta) + M*g*sin(theta))/(l*(M + m - m*cos(theta)**2)))
-    */
-
     Real const x[4] = {0.1, 0.2, 0.3, 0.4};
     Real const u[1] = {0.5};
     Real dx[4];
 
-    std::cout << "x: " << std::endl;
-    std::copy_n(x, 4, std::ostream_iterator<Real>(std::cout, ", "));
-    std::cout << std::endl;
-
-    std::cout << "u: ";
-    std::copy_n(u, 1, std::ostream_iterator<Real>(std::cout, ", "));
-    std::cout << std::endl;
-
-    f1(x, u, dx);
-    std::cout << "f1(x, u): ";
-    std::copy_n(dx, 4, std::ostream_iterator<Real>(std::cout, ", "));
-    std::cout << std::endl;
-
-    f2(x, u, dx);
-    std::cout << "f2(x, u): ";
-    std::copy_n(dx, 4, std::ostream_iterator<Real>(std::cout, ", "));
-    std::cout << std::endl;
-
-    std::size_t N = 1000000000;
-
-    {
-        std::cout << "Benchmarking f1()..." << std::endl;
-        auto const f1_start = clock::now();
-        for (std::size_t i = 0; i < N; ++i)
-            f1(x, u, dx);
-        auto const t1 = clock::now() - f1_start;
-        std::cout << "f1() time per evaluation: " << nanoseconds(t1).count() / N << " ns" << std::endl;
-    }
-
-    std::cout << "f1(x, u): ";
-    std::copy_n(dx, 4, std::ostream_iterator<Real>(std::cout, ", "));
-    std::cout << std::endl;
-
-    {
-        std::cout << "Benchmarking f2()..." << std::endl;
-        auto const f2_start = clock::now();
-        for (std::size_t i = 0; i < N; ++i)
-            f2(x, u, dx);
-        auto const t2 = clock::now() - f2_start;
-        std::cout << "f2() time per evaluation: " << nanoseconds(t2).count() / N << " ns" << std::endl;
-    }
-    
-    return 0;
+    while (state.KeepRunning())
+        f1(x, u, dx);
 }
+
+BENCHMARK(BM_f1);
+
+
+static void BM_f2(benchmark::State& state) 
+{
+    Real const x[4] = {0.1, 0.2, 0.3, 0.4};
+    Real const u[1] = {0.5};
+    Real dx[4];
+
+    while (state.KeepRunning())
+        f2(x, u, dx);
+}
+
+BENCHMARK(BM_f2);
+
+BENCHMARK_MAIN();
