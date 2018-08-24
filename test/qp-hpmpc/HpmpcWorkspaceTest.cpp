86a67bc9581bd111289bd587361d37a8eea9e03d
Mikhail Katliar <mikhail.katliar@tuebingen.mpg.de>
2018-08-24 16:49:27 +0200

    Added (but commented out) TreeQpWorkspaceSolveTest fpr hpmpc.

--- test/qp-hpmpc/HpmpcWorkspaceTest.cpp
+++ test/qp-hpmpc/HpmpcWorkspaceTest.cpp
@@ -1,6 +1,7 @@
 #include "QpWorkspaceTest.hpp"
-#include "TreeQpWorkspaceTest.hpp"
 #include "QpWorkspaceSolveTest.hpp"
+#include "TreeQpWorkspaceTest.hpp"
+#include "TreeQpWorkspaceSolveTest.hpp"
 
 #include <tmpc/qp/HpmpcWorkspace.hpp>
 #include <tmpc/qp/TreeQpWorkspaceAdaptor.hpp>
@@ -12,6 +13,7 @@ namespace tmpc :: testing
 	INSTANTIATE_TYPED_TEST_CASE_P(Hpmpc_Blaze_double, QpWorkspaceTest, HpmpcWorkspace<BlazeKernel<double>>);
 	INSTANTIATE_TYPED_TEST_CASE_P(Hpmpc_Blaze_double, QpWorkspaceSolveTest, HpmpcWorkspace<BlazeKernel<double>>);
 	INSTANTIATE_TYPED_TEST_CASE_P(Hpmpc_Blaze_double, TreeQpWorkspaceTest, TreeQpWorkspaceAdaptor<HpmpcWorkspace<BlazeKernel<double>>>);
+	//INSTANTIATE_TYPED_TEST_CASE_P(Hpmpc_Blaze_double, TreeQpWorkspaceSolveTest, TreeQpWorkspaceAdaptor<HpmpcWorkspace<BlazeKernel<double>>>);
 
 	INSTANTIATE_TYPED_TEST_CASE_P(Hpmpc_Eigen_double, QpWorkspaceTest, HpmpcWorkspace<EigenKernel<double>>);
 	INSTANTIATE_TYPED_TEST_CASE_P(Hpmpc_Eigen_double, QpWorkspaceSolveTest, HpmpcWorkspace<EigenKernel<double>>);
