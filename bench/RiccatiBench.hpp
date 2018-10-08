5963dd6ebad81e40408a661498e84deb2ae5ca86
Mikhail Katliar <cepstr@gmail.com>
2018-10-08 14:10:36 +0200

    Added Riccati benchmark analysis script.

--- bench/RiccatiBench.hpp
+++ bench/RiccatiBench.hpp
@@ -5,10 +5,5 @@
 
 namespace tmpc :: benchmark
 {
-    inline void riccatiBenchArguments(::benchmark::internal::Benchmark* b) 
-    {
-        for (int nx = 2; nx < 30; ++nx)
-            for (int nu = 1; nu < 10; ++nu)
-                b->Args({100, nx, nu});
-    }
+    void riccatiBenchArguments(::benchmark::internal::Benchmark* b);
 }
---
+++ bench/analyze_bench_riccati.m
@@ -0,0 +1,48 @@
+function analyze_bench_riccati(filename)
+% Analyze the results from Riccati solvers benchmark.
+
+    data = jsondecode(fileread(filename));
+    c = cell2mat(arrayfun(@parse_benchmark, data.benchmarks, 'UniformOutput', false));
+
+    for nu = unique([c.nu])
+        figure();
+        hold('on');
+
+        algs = {'Hpipm', 'Mpipm'};
+        for alg = algs
+            ind = strcmp({c.alg}, alg{:}) & strcmp({c.flag}, 'mean') & [c.nu] == nu;
+            plot([c(ind).nx], [c(ind).cpu_time]);
+        end
+
+        legend(algs);
+        title(sprintf('nu=%d', nu));
+        xlabel('nx');
+        ylabel('t [ns]');
+        grid('on');
+    end
+end
+
+
+function s = parse_benchmark(b)
+    s.cpu_time = b.cpu_time;
+    [s.alg, s.N, s.nx, s.nu, s.flag] = parse_benchmark_name(b.name);
+end
+
+
+% function s = parse_benchmark_name(str)
+%     parts = strsplit(str, {'/', '_', 'Riccati'});
+%     s.alg = parts{2};
+%     s.N = str2double(parts{3});
+%     s.nx = str2double(parts{4});
+%     s.nu = str2double(parts{5});
+%     s.value = parts{6};
+% end
+
+function [alg, N, nx, nu, flag] = parse_benchmark_name(str)
+    parts = strsplit(str, {'/', '_', 'Riccati'});
+    alg = parts{2};
+    N = str2double(parts{3});
+    nx = str2double(parts{4});
+    nu = str2double(parts{5});
+    flag = parts{6};
+end
