01b43735dee4d673bcc289fb1980dfa7f60f38d6
Mikhail Katliar <mikhail.katliar@rozum.com>
2019-02-14 16:00:45 +0300

    Added MultiStepIntegrator test based on pendulum pre-calculated data.

--- test/integrator/MultiStepIntegratorTest.cpp
+++ test/integrator/MultiStepIntegratorTest.cpp
@@ -1,5 +1,8 @@
 #include <tmpc/integrator/ExplicitRungeKutta4.hpp>
 #include <tmpc/integrator/MultiStepIntegrator.hpp>
+
+#include "PendulumData.hpp"
+
 #include <tmpc/test_tools.hpp>
 
 
@@ -14,7 +17,7 @@ namespace tmpc :: testing
 	}
 
 
-	TEST(MultiStepIntegratorTest, testAccuracy)
+	TEST(MultiStepIntegratorSimpleTest, testAccuracy)
 	{
 		double const h = 1.;
 		double const h_max = 0.5;
@@ -33,5 +36,57 @@ namespace tmpc :: testing
 		double const err_multistep = abs(x1_multistep[0] - x1_true[0]);
 
 		EXPECT_GE(err_rk4 / err_multistep, 0.6 * pow(h / h_max, 4));
+	}
+
+
+	class MultiStepIntegratorTest 
+	: 	public ::testing::Test
+	{
+	protected:
+		using Real = double;
+		typedef PendulumOdeBase ODE;
+
+		PendulumOde ode_;
+		PendulumOdeR odeR_;
+		ExplicitRungeKutta4<Real> rk4_ {ODE::NX, ODE::NU};
+		MultiStepIntegrator<Real> integrator_ {ODE::NX, ODE::NU};
+
+		std::vector<TestPoint> test_data_;
+
+		void SetUp() override
+		{
+			test_data_ = loadPendulumData();
+			ASSERT_EQ(test_data_.size(), 600);
+		}
+	};
+
+
+	TEST_F(MultiStepIntegratorTest, testIntegrate)
+	{
+		for (auto p : test_data_)
+		{
+			ODE::StateVector const xplus = integrator_(rk4_, ode_, p.t, p.x0, p.u, p.timeStep, p.timeStep * 0.1);
+
+			// Tolerance increased by the factor of 1e+2, because the test data are obtained using 1-step RK4.
+			MatrixApproxEquality const is_approx(1e-8);
+			EXPECT_PRED2(is_approx, forcePrint(xplus), forcePrint(p.xplus));
+		}
+	}
+
+
+	TEST_F(MultiStepIntegratorTest, testIntegrateWithSensitivities)
+	{
+		for (auto p : test_data_)
+		{
+			ODE::StateVector xplus;
+			ODE::StateStateMatrix A;
+			ODE::StateInputMatrix B;
+			integrator_(rk4_, ode_, p.t, p.x0, p.u, p.timeStep, p.timeStep * 0.1, xplus, A, B);
+
+			// Tolerance increased by the factor of 1e+2, because the test data are obtained using 1-step RK4.
+			EXPECT_PRED2(MatrixApproxEquality(1e-8), forcePrint(xplus), forcePrint(p.xplus));
+			EXPECT_PRED2(MatrixApproxEquality(1e-8), forcePrint(A), forcePrint(p.A));
+			EXPECT_PRED2(MatrixApproxEquality(1e-8), forcePrint(B), forcePrint(p.B));
+		}
 	}
 }
