8b31a3fa3d351f69617d6aa9fa7a44dabd7bccff
Mikhail Katliar <cepstr@gmail.com>
2019-05-16 12:32:57 +0200

    Re-wrote NewtonSolver test end example using generic lambdas.

--- examples/numeric/newton_solver.cpp
+++ examples/numeric/newton_solver.cpp
@@ -2,46 +2,7 @@
 
 #include <tmpc/numeric/NewtonSolver.hpp>
 
-#include <cmath>
 #include <iostream>
-
-
-/// @brief Functor for the residual function
-struct Fun
-{
-    template <typename VT1, typename VT2, typename MT, bool SO>
-    void operator()(blaze::Vector<VT1, blaze::columnVector> const& x, 
-        blaze::Vector<VT2, blaze::columnVector>& f, blaze::Matrix<MT, SO>& J) const
-    {
-        ~f = {
-            std::pow((~x)[0], 2) + std::pow((~x)[1], 3) - 1.,
-            2. * (~x)[0] + 3. * std::pow((~x)[1], 2) - 4.
-        };
-
-        ~J = {
-            {2. * (~x)[0], 3. * std::pow((~x)[1], 2)},
-            {2., 6. * (~x)[1]}
-        };
-    };
-};
-
-
-/// @brief Iteration monitor
-struct Monitor
-{
-    template <typename VT1, typename VT2, typename MT, bool SO>
-    void operator()(size_t iter, 
-        blaze::Vector<VT1, blaze::columnVector> const& x,
-        blaze::Vector<VT2, blaze::columnVector> const& r,
-        blaze::Matrix<MT, SO> const& J) const
-    {
-        std::cout << "iteration " << iter << std::endl;
-        std::cout << "x = " << trans(~x);
-        std::cout << "r = " << trans(~r);
-        std::cout << "J = " << std::endl << J;
-        std::cout << "----------------------------" << std::endl;
-    }
-};
 
 
 int main(int, char **)
@@ -54,17 +15,37 @@ int main(int, char **)
 
     // Newton solver
     tmpc::NewtonSolver<Real> solver(NX);
-    solver.maxIterations(20);
+    solver.maxIterations(10);
 
-    // The function to minimize
-    Fun fun;
+    // Define the equation and its jacobian
+    auto fun = [] (auto const& x, auto& f, auto& J)        
+    {
+        ~f = {
+            std::pow(x[0], 2) + std::pow(x[1], 3) - 1.,
+            2. * x[0] + 3. * std::pow(x[1], 2) - 4.
+        };
 
-    // Initial guess
+        ~J = {
+            {2. * x[0], 3. * std::pow(x[1], 2)},
+            {2., 6. * x[1]}
+        };
+    };
+
+    // Initial point
     Vec const x0 {-2., -1.};
-    
-    // Find the solution
-    Vec const x_star = solver.solve(fun, x0, Monitor());
 
+     // Monitor function
+    auto monitor = [] (size_t iter, auto const& x, auto const& r, auto const& J)
+    {
+        std::cout << "iteration " << iter << std::endl;
+        std::cout << "x = " << trans(~x);
+        std::cout << "r = " << trans(~r);
+        std::cout << "J = " << std::endl << J;
+        std::cout << "----------------------------" << std::endl;
+    };
+
+    // Find the solution
+    Vec const x_star = solver.solve(fun, x0, monitor);
     std::cout << "Solution found at " << trans(x_star);
 
     return 0;
--- test/numeric/NewtonSolverTest.cpp
+++ test/numeric/NewtonSolverTest.cpp
@@ -1,41 +1,11 @@
 #include <tmpc/numeric/NewtonSolver.hpp>
 #include <tmpc/Testing.hpp>
 
-#include <cmath>
-
 
 namespace tmpc :: testing
 {
-    class NewtonSolverTest
-    :   public Test
-    {
-    protected:
-        // Test functor
-        struct Fun
-        {
-            template <typename VT1, typename VT2, typename MT, bool SO>
-            void operator()(blaze::Vector<VT1, blaze::columnVector> const& x, 
-                blaze::Vector<VT2, blaze::columnVector>& f, blaze::Matrix<MT, SO>& J) const
-            {
-                ~f = {
-                    pow((~x)[0], 2) + pow((~x)[1], 3) - 1.,
-                    2. * (~x)[0] + 3. * pow((~x)[1], 2) - 4.
-                };
-
-                ~J = {
-                    {2. * (~x)[0], 3. * pow((~x)[1], 2)},
-                    {2., 6. * (~x)[1]}
-                };
-            };
-        };
-
-
-        Fun fun_;
-    };
-
-
     /// @brief Check that the Newton method finds the correct solution of a system of 2 equations.
-    TEST_F(NewtonSolverTest, testSolve)
+    TEST(NewtonSolverTest, testSolve)
     {
         size_t constexpr NX = 2;
         
@@ -45,13 +15,26 @@ namespace tmpc :: testing
 
         // Newton solver
         NewtonSolver<Real> solver(NX);
-        solver.maxIterations(20);
+
+        // Define the equation and its Jacobian
+        auto fun = [] (auto const& x, auto& f, auto& J)
+        {
+            ~f = {
+                pow(x[0], 2) + pow(x[1], 3) - 1.,
+                2. * x[0] + 3. * pow(x[1], 2) - 4.
+            };
+
+            ~J = {
+                {2. * x[0], 3. * pow(x[1], 2)},
+                {2., 6. * x[1]}
+            };
+        };
 
         // Initial guess
         Vec const x0 {-2., -1.};
 
         // Find the solution
-        Vec const x_star = solver.solve(fun_, x0);
+        Vec const x_star = solver.solve(fun, x0);
         
         // Check the solution
         TMPC_EXPECT_APPROX_EQ(x_star, (Vec {-2.48345, -1.72886}), 1.e-5, 0.);
@@ -59,7 +42,7 @@ namespace tmpc :: testing
         // Check residual at the solution
         Vec r;
         Mat J;
-        fun_(x_star, r, J);
+        fun(x_star, r, J);
         EXPECT_LT(maxNorm(r), solver.residualTolerance());
         EXPECT_EQ(solver.residualMaxNorm(), maxNorm(r));
 
