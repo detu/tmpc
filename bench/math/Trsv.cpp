f738a3a6a200a29e55efc2c0c8001d62524f638c
Mikhail Katliar <cepstr@gmail.com>
2019-09-15 12:10:51 +0200

    Added tmpc::trsv() function

--- bench/math/Trsv.cpp
+++ bench/math/Trsv.cpp
@@ -1,6 +1,8 @@
 #include <blaze/Math.h>
 #include <Eigen/Dense>
 
+#include <tmpc/math/Trsv.hpp>
+
 #include <benchmark/benchmark.h>
 
 
@@ -37,14 +39,50 @@ namespace tmpc :: benchmark
     }
 
 
+    template <typename Real, size_t M, bool SO>
+    static void BM_trsv_Lower_tmpc_Static(::benchmark::State& state)
+    {
+        blaze::LowerMatrix<blaze::StaticMatrix<Real, M, M, SO>> A;        
+        blaze::StaticVector<Real, M, blaze::columnVector> b;
+        blaze::StaticVector<Real, M, blaze::columnVector> x;
+
+        randomize(A);
+        randomize(b);
+        
+        for (auto _ : state)
+        {
+            tmpc::trsv(A, b, x);
+            ::benchmark::DoNotOptimize(x[M - 1]);
+        }
+    }
+
+
+    template <typename Real, size_t M, bool SO>
+    static void BM_trsv_TransLower_tmpc_Static(::benchmark::State& state)
+    {
+        blaze::LowerMatrix<blaze::StaticMatrix<Real, M, M, SO>> A;        
+        blaze::StaticVector<Real, M, blaze::rowVector> b;
+        blaze::StaticVector<Real, M, blaze::rowVector> x;
+
+        randomize(A);
+        randomize(b);
+        
+        for (auto _ : state)
+        {
+            tmpc::trsv(b, A, x);
+            ::benchmark::DoNotOptimize(x[M - 1]);
+        }
+    }
+
+
 #if BLAZE_BLAS_MODE
-    template <typename Real>
+    template <typename Real, bool SO>
     static void BM_trsv_Blaze_blas(::benchmark::State& state)
     {
         size_t const M = state.range(0);
-        blaze::DynamicMatrix<Real, blaze::columnMajor> A(M, M);        
-        blaze::DynamicVector<Real, blaze::columnVector> B(M);
-        blaze::DynamicVector<Real, blaze::columnVector> C(M);
+        blaze::DynamicMatrix<Real, SO> A(M, M);        
+        blaze::DynamicVector<Real, SO> B(M);
+        blaze::DynamicVector<Real, SO> C(M);
 
         randomize(A);
         randomize(B);
@@ -60,16 +98,39 @@ namespace tmpc :: benchmark
 
     static void trsvBenchArguments(::benchmark::internal::Benchmark* b) 
     {
-        b->Arg(1)->Arg(4)->Arg(35);
+        b->Arg(1)->Arg(4)->Arg(35)->Arg(60);
     }
 
 
     BENCHMARK_TEMPLATE(BM_trsv_Blaze_Dynamic, double)->Apply(trsvBenchArguments);
+    
     BENCHMARK_TEMPLATE(BM_trsv_Blaze_Static, double, 1);
     BENCHMARK_TEMPLATE(BM_trsv_Blaze_Static, double, 4);
     BENCHMARK_TEMPLATE(BM_trsv_Blaze_Static, double, 35);
+    BENCHMARK_TEMPLATE(BM_trsv_Blaze_Static, double, 60);
+
+    BENCHMARK_TEMPLATE(BM_trsv_Lower_tmpc_Static, double, 1, blaze::rowMajor);
+    BENCHMARK_TEMPLATE(BM_trsv_Lower_tmpc_Static, double, 4, blaze::rowMajor);
+    BENCHMARK_TEMPLATE(BM_trsv_Lower_tmpc_Static, double, 35, blaze::rowMajor);
+    BENCHMARK_TEMPLATE(BM_trsv_Lower_tmpc_Static, double, 60, blaze::rowMajor);
+
+    BENCHMARK_TEMPLATE(BM_trsv_Lower_tmpc_Static, double, 1, blaze::columnMajor);
+    BENCHMARK_TEMPLATE(BM_trsv_Lower_tmpc_Static, double, 4, blaze::columnMajor);
+    BENCHMARK_TEMPLATE(BM_trsv_Lower_tmpc_Static, double, 35, blaze::columnMajor);
+    BENCHMARK_TEMPLATE(BM_trsv_Lower_tmpc_Static, double, 60, blaze::columnMajor);
+
+    BENCHMARK_TEMPLATE(BM_trsv_TransLower_tmpc_Static, double, 1, blaze::rowMajor);
+    BENCHMARK_TEMPLATE(BM_trsv_TransLower_tmpc_Static, double, 4, blaze::rowMajor);
+    BENCHMARK_TEMPLATE(BM_trsv_TransLower_tmpc_Static, double, 35, blaze::rowMajor);
+    BENCHMARK_TEMPLATE(BM_trsv_TransLower_tmpc_Static, double, 60, blaze::rowMajor);
+
+    BENCHMARK_TEMPLATE(BM_trsv_TransLower_tmpc_Static, double, 1, blaze::columnMajor);
+    BENCHMARK_TEMPLATE(BM_trsv_TransLower_tmpc_Static, double, 4, blaze::columnMajor);
+    BENCHMARK_TEMPLATE(BM_trsv_TransLower_tmpc_Static, double, 35, blaze::columnMajor);
+    BENCHMARK_TEMPLATE(BM_trsv_TransLower_tmpc_Static, double, 60, blaze::columnMajor);
 
 #if BLAZE_BLAS_MODE
-    BENCHMARK_TEMPLATE(BM_trsv_Blaze_blas, double)->Apply(trsvBenchArguments);
+    BENCHMARK_TEMPLATE(BM_trsv_Blaze_blas, double, blaze::rowMajor)->Apply(trsvBenchArguments);
+    BENCHMARK_TEMPLATE(BM_trsv_Blaze_blas, double, blaze::columnMajor)->Apply(trsvBenchArguments);
 #endif
 }
