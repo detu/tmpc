42d87e1edb3989a46ac29da311e4e2430ed3c570
Mikhail Katliar <cepstr@gmail.com>
2019-10-03 09:25:12 +0300

    * Implemented interface to BLASFEO gemm_nt() * Changed gemm_nn() to gemm_nt() in BLASFEO gemm benchmark

--- test/blasfeo/GemmTest.cpp
+++ test/blasfeo/GemmTest.cpp
@@ -72,4 +72,38 @@ namespace tmpc :: testing
 
         TMPC_EXPECT_APPROX_EQ(blaze_blasfeo_D, blaze_D, 1e-10, 1e-10);
     }
+
+
+    TEST(BlasfeoTest, testGemmNT)
+    {
+        size_t const m = 5, n = 6, k = 4;
+
+        // Init Blaze matrices
+        //
+        blaze::DynamicMatrix<double, blaze::columnMajor> blaze_A(m, k), blaze_B(n, k), blaze_C(m, n), blaze_D(m, n);
+        randomize(blaze_A);
+        randomize(blaze_B);
+        randomize(blaze_C);
+
+        // Do gemm with Blaze
+        //
+        blaze_D = blaze_C + blaze_A * trans(blaze_B);
+        // std::cout << "blaze_D=\n" << blaze_D;
+
+        // Init BLASFEO matrices
+        //
+        blasfeo::DynamicMatrix<double> blasfeo_A(blaze_A), blasfeo_B(blaze_B), blasfeo_C(blaze_C), blasfeo_D(m, n);
+        
+        // Do gemm with BLASFEO
+        gemm_nt(m, n, k, 1.0, blasfeo_A, 0, 0, blasfeo_B, 0, 0, 1.0, blasfeo_C, 0, 0, blasfeo_D, 0, 0);
+
+        // Copy the resulting D matrix from BLASFEO to Blaze
+        blaze::DynamicMatrix<double, blaze::columnMajor> blaze_blasfeo_D;
+        blasfeo_D.unpack(blaze_blasfeo_D);
+
+        // Print the result from BLASFEO
+        // std::cout << "blaze_D=\n" << blaze_blasfeo_D;
+
+        TMPC_EXPECT_APPROX_EQ(blaze_blasfeo_D, blaze_D, 1e-10, 1e-10);
+    }
 }
--- tmpc/blasfeo/Gemm.hpp
+++ tmpc/blasfeo/Gemm.hpp
@@ -84,4 +84,42 @@ namespace tmpc :: blasfeo
             const_cast<blasfeo_smat *>(&sC), ci, cj, 
             &sD, di, dj);
     }
+
+
+    /// @brief D <= beta * C + alpha * A * B^T
+    inline void gemm_nt(size_t m, size_t n, size_t k,
+        double alpha,
+        blasfeo_dmat const& sA, size_t ai, size_t aj,
+        blasfeo_dmat const& sB, size_t bi, size_t bj,
+        double beta,
+        blasfeo_dmat const& sC, size_t ci, size_t cj,
+        blasfeo_dmat& sD, size_t di, size_t dj)
+    {
+        blasfeo_dgemm_nt(m, n, k, 
+            alpha, 
+            const_cast<blasfeo_dmat *>(&sA), ai, aj, 
+            const_cast<blasfeo_dmat *>(&sB), bi, bj, 
+            beta, 
+            const_cast<blasfeo_dmat *>(&sC), ci, cj, 
+            &sD, di, dj);
+    }
+
+
+    /// @brief D <= beta * C + alpha * A * B^T
+    inline void gemm_nt(size_t m, size_t n, size_t k,
+        float alpha,
+        blasfeo_smat const& sA, size_t ai, size_t aj,
+        blasfeo_smat const& sB, size_t bi, size_t bj,
+        float beta,
+        blasfeo_smat const& sC, size_t ci, size_t cj,
+        blasfeo_smat& sD, size_t di, size_t dj)
+    {
+        blasfeo_sgemm_nt(m, n, k, 
+            alpha, 
+            const_cast<blasfeo_smat *>(&sA), ai, aj, 
+            const_cast<blasfeo_smat *>(&sB), bi, bj, 
+            beta, 
+            const_cast<blasfeo_smat *>(&sC), ci, cj, 
+            &sD, di, dj);
+    }
 }
