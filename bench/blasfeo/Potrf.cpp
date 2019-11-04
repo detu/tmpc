b9fba393bb12544fd19717c5acde49ba2ecbd330
Mikhail Katliar <cepstr@gmail.com>
2019-11-04 12:50:48 +0300

    Templated version of blasfeo potrf benchmark.

--- bench/blasfeo/Potrf.cpp
+++ bench/blasfeo/Potrf.cpp
@@ -7,24 +7,31 @@
 
 namespace tmpc :: benchmark
 {
+    using namespace ::benchmark;
+
+
+    template <typename T>
     void BM_potrf(::benchmark::State& state)
     {
         size_t const m = state.range(0);
 
         // Init Blaze matrices
         //
-        blaze::DynamicMatrix<double, blaze::columnMajor> blaze_A(m, m), blaze_L(m, m);
+        blaze::DynamicMatrix<T, blaze::columnMajor> blaze_A(m, m), blaze_L(m, m);
         makePositiveDefinite(blaze_A);
         
         // Init BLASFEO matrices
         //
-        blasfeo::DynamicMatrix<double> blasfeo_A(blaze_A), blasfeo_L(m, m);
+        blasfeo::DynamicMatrix<T> blasfeo_A(blaze_A), blasfeo_L(m, m);
         
         // Do potrf with BLASFEO
         for (auto _ : state)
             potrf(m, blasfeo_A, 0, 0, blasfeo_L, 0, 0);
+
+        state.counters["flops"] = Counter(m * m * m / 3., Counter::kIsIterationInvariantRate);
+        state.counters["m"] = m;
     }
 
 
-    BENCHMARK(BM_potrf)->DenseRange(1, 50);
+    BENCHMARK_TEMPLATE(BM_potrf, double)->DenseRange(1, 300);
 }
