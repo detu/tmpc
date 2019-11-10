1261964c84f3dbf4aebdf58ae21601b4c6c1dc46
Mikhail Katliar <cepstr@gmail.com>
2019-11-10 11:15:43 +0300

    More accutate calculation of flops in blasfeo potrf benchmark.

--- bench/blasfeo/Potrf.cpp
+++ bench/blasfeo/Potrf.cpp
@@ -14,6 +14,7 @@ namespace tmpc :: benchmark
     void BM_potrf(::benchmark::State& state)
     {
         size_t const m = state.range(0);
+        size_t const n = m;
 
         // Init Blaze matrices
         //
@@ -28,7 +29,8 @@ namespace tmpc :: benchmark
         for (auto _ : state)
             potrf(m, blasfeo_A, 0, 0, blasfeo_L, 0, 0);
 
-        state.counters["flops"] = Counter(m * m * m / 3., Counter::kIsIterationInvariantRate);
+        // Calculated as \sum _{k=0}^{n-1} \sum _{j=0}^{k-1} \sum _{i=k}^{m-1} 2
+        state.counters["flops"] = Counter((1 + 3 * m - 2 * n) * (n - 1) * n / 3, Counter::kIsIterationInvariantRate);
         state.counters["m"] = m;
     }
 
